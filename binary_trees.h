9eda7b457bced0e3cde429706d2923351d4dff5f
Hussam <samdevy@outlook.com>
2025-10-30 14:08:43 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -5,30 +5,42 @@
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
- *
- * Description: Binary tree node structure for Holberton project
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
-
-/* Function prototypes */
+/* 0. New node */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/* 1. Insert left */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/* 2. Insert right */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/* 3. Delete tree */
 void binary_tree_delete(binary_tree_t *tree);
+
+/* 4. Is leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
+
+/* 5. Is root */
 int binary_tree_is_root(const binary_tree_t *node);
+
+/* 6. Pre-order traversal */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+
+/* Optional print function (provided by Holberton) */
 void binary_tree_print(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
