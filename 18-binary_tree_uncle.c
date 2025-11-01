148deb6fe16fc96e6f37420d8466fbf695c3f799
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 14:12:14 +0000

    task18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -4,12 +4,14 @@
  * binary_tree_uncle - finds the uncle of a node
  * @node: pointer to the node
  *
- * Return: pointer to uncle, or NULL if none exists
+ * Return: pointer to uncle node, or NULL if no uncle exists
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
+	/* check if node, parent, or grandparent is NULL */
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
+	/* uncle is the sibling of the parent */
 	return (binary_tree_sibling(node->parent));
 }
