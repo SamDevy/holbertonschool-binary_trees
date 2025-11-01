5e757533514c1254e1b8a26dd0acc4f4b7fad711
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 13:48:26 +0000

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,18 +8,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    if (!tree)
-        return 0;
+	if (!tree)
+		return (0);
 
-    left_height = binary_tree_height(tree->left);
-    right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-    if (left_height > right_height)
-        return left_height + 1;
-    else
-        return right_height + 1;
+	if (left_height > right_height)
+		return (left_height + 1);
+	return (right_height + 1);
 }
 
 /**
@@ -30,13 +29,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (!tree)
-        return 0;
+	if (!tree)
+		return (0);
 
-    left_height = (int)binary_tree_height(tree->left);
-    right_height = (int)binary_tree_height(tree->right);
+	left_height = (int)binary_tree_height(tree->left);
+	right_height = (int)binary_tree_height(tree->right);
 
-    return left_height - right_height;
+	return (left_height - right_height);
 }
