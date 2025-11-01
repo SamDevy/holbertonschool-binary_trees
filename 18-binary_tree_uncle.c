044a9e53b451b973a7c7293bb27f4a52a4bcef1e
AlanoudAlharthi <Alanoud_Alharthi@outlook.com>
2025-11-01 14:10:18 +0000

    task18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -2,9 +2,9 @@
 
 /**
  * binary_tree_uncle - finds the uncle of a node
- * @node: pointer to the node to find the uncle for
+ * @node: pointer to the node
  *
- * Return: pointer to the uncle node, or NULL if no uncle exists
+ * Return: pointer to uncle, or NULL if none exists
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
